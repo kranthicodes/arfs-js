d46172fbb465b4d35a222d4bdd6d371126914530
Sai Kranthi <iamsaikranthi@gmail.com>
2023-09-10 19:21:17 +0530

    feat: tsup project init

---
+++ tsup.config.ts
@@ -0,0 +1,20 @@
+import type { Options } from "tsup";
+
+const env = process.env.NODE_ENV;
+
+export const tsup: Options = {
+  splitting: true,
+  clean: true, // clean up the dist folder
+  dts: true, // generate dts files
+  format: ["cjs", "esm", "iife"], // generate cjs, iife and esm files
+  minify: env === "production",
+  bundle: env === "production",
+  skipNodeModulesBundle: true,
+  entryPoints: ["src/index.ts"],
+  watch: env === "development",
+  target: "es2020",
+  outDir: "dist",
+  entry: ["src/**/*.ts"], //include all files under src
+  shims: true,
+  sourcemap: true,
+};
